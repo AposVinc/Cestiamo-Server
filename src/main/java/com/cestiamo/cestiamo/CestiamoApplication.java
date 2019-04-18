0bd93b90e3256cbe9363eb1a946144748598bf27
AposVinc <apos.vincenzo@gmail.com>
2019-04-18 17:42:47 +0200

    Initial commit

---
+++ src/main/java/com/cestiamo/cestiamo/CestiamoApplication.java
@@ -0,0 +1,13 @@
+package com.cestiamo.cestiamo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CestiamoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CestiamoApplication.class, args);
+	}
+
+}
